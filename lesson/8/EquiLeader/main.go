14da7f159dde348a8b70cd28af06251bfd1caa3e
tkido <tkido@xevo.com>
2023-09-18 16:04:11 +0900

    EquiLeader

--- lesson/8/EquiLeader/main.go
+++ lesson/8/EquiLeader/main.go
@@ -1,48 +1,40 @@
 package main
 
-const null = -1000000001
-
 func Solution(A []int) int {
-	m := make(map[int]int)
-	leftLeaders := make([]int, len(A))
-	leader := null
-	leaderCount := 0
-	for i, v := range A {
-		m[v]++
-		if m[v] > (i+1)/2 {
-			leader = v
-			leaderCount = m[v]
-			leftLeaders[i] = v
-		} else if leaderCount <= (i+1)/2 {
-			leader = null
-			leftLeaders[i] = null
+	leader := 0
+	size := 0
+	value := 0
+	for _, v := range A {
+		if size == 0 {
+			size++
+			value = v
 		} else {
-			leftLeaders[i] = leader
+			if value != v {
+				size--
+			} else {
+				size++
+			}
 		}
 	}
-	m = make(map[int]int)
-	rightLeaders := make([]int, len(A))
-	leader = null
-	leaderCount = 0
-	for i := len(A) - 1; i >= 0; i-- {
-		v := A[i]
-		m[v]++
-		if m[v] > (len(A)-i)/2 {
-			leader = v
-			leaderCount = m[v]
-			rightLeaders[i] = v
-		} else if leaderCount <= (len(A)-i)/2 {
-			leader = null
-			rightLeaders[i] = null
-		} else {
-			rightLeaders[i] = leader
-		}
+	if size > 0 {
+		leader = value
 	}
+	counts := make([]int, len(A))
 	count := 0
-	for i := 0; i < len(A)-1; i++ {
-		if leftLeaders[i] != null && leftLeaders[i] == rightLeaders[i+1] {
+	for i, v := range A {
+		if v == leader {
 			count++
 		}
+		counts[i] = count
 	}
-	return count
+	if count <= len(A)/2 {
+		return 0
+	}
+	rst := 0
+	for i, _ := range A {
+		if counts[i] > (i+1)/2 && count-counts[i] > (len(A)-i-1)/2 {
+			rst++
+		}
+	}
+	return rst
 }
